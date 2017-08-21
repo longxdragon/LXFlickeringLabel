ac461fa680cf7301efad366929bf5d46f81f29af
gkoudai_xulong <longxdragon@163.com>
2017-08-21 14:14:51 +0800

    Fix warning

--- LXFlickeringLabel/LXFlickeringLabel.m
+++ LXFlickeringLabel/LXFlickeringLabel.m
@@ -8,7 +8,7 @@
 
 #import "LXFlickeringLabel.h"
 
-@interface LXFlickeringLabel ()
+@interface LXFlickeringLabel () <CAAnimationDelegate>
 
 @property (nonatomic, strong) CAGradientLayer *gradientLayer;
 
